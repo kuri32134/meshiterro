f90fade9b80ccc917d4859f86283bf1025c79ed8
kuri32134 <161860576+kuri32134@users.noreply.github.com>
2024-05-09 07:55:30 +0000

    管理者機能を実装しよう-管理者としての機能追加,構成の改善

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,21 @@ Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations, :password], controllers: {
     sessions: 'admin/sessions'
   }
-  devise_for :users
-  root to: 'homes#top'
-  get "homes/about" => "homes#about" , as: "about"
-  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
-    resource :favorite, only: [:create, :destroy]
-    resources :post_comments, only: [:create, :destroy]
+
+  namespace :admin do
+    get 'dashboards', to: 'dashboards#index'
+    resources :users, only: [:destroy]
   end
-  resources :users, only: [:show, :edit, :update]
+
+  scope module: :public do
+    devise_for :users
+    root to: 'homes#top'
+    get "homes/about" => "homes#about" , as: "about"
+    resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+      resource :favorites, only: [:create, :destroy]
+      resources :post_comments, only: [:create, :destroy]
+    end
+    resources :users, only: [:show, :edit, :update]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
